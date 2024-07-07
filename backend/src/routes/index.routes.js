b7d570d14e6ad1d6365d80b24ba6c3822a52dc45
Kaito <dilan.aranguiz2201@alumnos.ubiobio.cl>
2024-07-07 06:45:50 -0400

    MOD: Enrutador principal para incluir nuevas rutas

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -8,12 +8,22 @@ import userRoutes from "./user.routes.js";
 /** Enrutador de autenticación */
 import authRoutes from "./auth.routes.js";
 
+/** Enrutador de likes */
+import likeRoutes from "./like.routes.js";
+
+/** Enrutador de matches */
+import matchRoutes from "./match.routes.js";
+
 // Se realiza una instancia de express
 const router = Router();
 
 // Define las rutas para los usuarios /api/users
-router.use("/user",  userRoutes);
+router.use("/user", userRoutes);
 // Define las rutas para la autenticación /api/auth
 router.use("/auth", authRoutes);
+// Define las rutas para los likes /api/likes
+router.use("/likes", likeRoutes);
+// Define las rutas para los matches /api/matches
+router.use("/matches", matchRoutes);
 
 export default router;

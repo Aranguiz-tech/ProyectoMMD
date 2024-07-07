5cf33868dec9898751714873e75538ed2f5357b1
Kaito <dilan.aranguiz2201@alumnos.ubiobio.cl>
2024-07-07 06:45:39 -0400

    MOD: Rutas de autenticación para incluir registro de administrador

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,20 +1,14 @@
 "use strict";
 
-// Se importa el módulo de express
 import express from "express";
-// Se importa las funciones del controlador
-import { login, register, profile, logout } from "../controllers/auth.controller.js";
+import { login, register, registerAdmin, profile, logout } from "../controllers/auth.controller.js";
 
-// Se realiza una instancia de express
 const router = express.Router();
 
-// Petición de tipo post para la ruta del login
 router.post("/login", login);
-// Petición de tipo post para la ruta del register
 router.post("/register", register);
-// Petición de tipo get para la ruta del profile
+router.post("/register-admin", registerAdmin); 
 router.get("/profile", profile);
-// Petición de tipo post para la ruta del logout
 router.post("/logout", logout);
 
 export default router;

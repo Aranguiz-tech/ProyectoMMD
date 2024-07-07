40a00bce7906c903d7b1aca36fa3f56eb7b6c9ff
Kaito <dilan.aranguiz2201@alumnos.ubiobio.cl>
2024-07-07 06:44:59 -0400

    MOD: Funcionalidad de dislike y ajustes en el controlador de usuario

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,52 +2,6 @@
 import User from '../models/user.model.js';
 import Role from '../models/role.model.js';
 
-// Función para dar "like" a un usuario
-export const likeUser = async (req, res) => {
-    try {
-        const { userId, likedUserId } = req.body;  // obtener los IDs de los usuarios
-
-        const user = await User.findById(userId);  // encontrar el usuario que da el like
-        const likedUser = await User.findById(likedUserId);  // encontrar el usuario que recibe el like
-
-        if (!user || !likedUser) {
-            return res.status(404).json({ message: "Usuario no encontrado" });  // 404: no encontrado, verificar que ambos usuarios existen
-        }
-
-        if (!user.likes.includes(likedUserId)) { //esta condicion nos asegura qwe no hayan duplicados
-            user.likes.push(likedUserId);  // Agregar el ID del usuario liked si no está ya en la lista
-            await user.save();  // guardar los cambios
-        }
-
-        res.status(200).json({ message: "Usuario liked exitosamente" });  // 200 es el exito, hablando de numeros
-    } catch (error) {
-        res.status(500).json({ message: "Error en el servidor", error });  // 500 no se cumplió una condicion, para que el cliente sepa que la solicitud no se pudo completar debido a un problema interno
-    }
-};
-
-// Función para dar "dislike" a un usuario
-export const dislikeUser = async (req, res) => {
-    try {
-        const { userId, dislikedUserId } = req.body;  // Obtener los IDs de los usuarios
-
-        const user = await User.findById(userId);  // Encontrar el usuario que da el dislike
-        const dislikedUser = await User.findById(dislikedUserId);  // Encontrar el usuario que recibe el dislike
-
-        if (!user || !dislikedUser) {
-            return res.status(404).json({ message: "Usuario no encontrado" });  // Verificar que ambos usuarios existen
-        }
-
-        if (!user.dislikes.includes(dislikedUserId)) {
-            user.dislikes.push(dislikedUserId);  // Agregar el ID del usuario disliked si no está ya en la lista
-            await user.save();  // Guardar los cambios
-        }
-
-        res.status(200).json({ message: "Usuario disliked exitosamente" });  // Responder con éxito
-    } catch (error) {
-        res.status(500).json({ message: "Error en el servidor", error });  // Manejar errores
-    }
-};
-
 export async function getUser(req, res) {
     try {
         const rutUser = req.query.rut;

4ca143e14eb79f2e50b55a154926f27cdfe315b2
Kaito <dilan.aranguiz2201@alumnos.ubiobio.cl>
2024-07-09 02:25:03 -0400

    FIX: Ajustes en el enrutador principal para manejar rutas protegidas

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,14 +1,14 @@
 "use strict";
-//organiza todas las rutas de tu API, redirigiendo las solicitudes a los controladores adecuados
+
 // Importa el modulo 'express' para crear las rutas
 import { Router } from "express";
 
-/** Enrutadores de usuarios, autenticación, likes y dislikes, matches */
+/** Enrutadores de usuarios, autenticación, likes y matches */
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
 import likeRoutes from "./like.routes.js";
-import dislikeRoutes from "./dislike.routes.js";
 import matchRoutes from "./match.routes.js";
+import dislikeRoutes from "./dislike.routes.js";
 
 // Se realiza una instancia de express
 const router = Router();
@@ -17,11 +17,11 @@ const router = Router();
 router.use("/user", userRoutes);
 // Define las rutas para la autenticación /api/auth
 router.use("/auth", authRoutes);
-// Define las rutas para los likes y dislikes /api/like
+// Define las rutas para los likes /api/like
 router.use("/like", likeRoutes);
+// Define las rutas para los dislikes /api/dislike
 router.use("/dislike", dislikeRoutes);
 // Define las rutas para los matches /api/match
 router.use("/match", matchRoutes);
 
 export default router;
-

4d2af14c68f186a86bacf3f733d678f784cf6ec5
Kaito <dilan.aranguiz2201@alumnos.ubiobio.cl>
2024-07-09 02:24:43 -0400

    FIX: Verificación de autenticación y roles de administrador en middlewares

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,34 +5,35 @@
  * @param {Function} next - Función para continuar con la siguiente función de middleware
  */
 function isAuthenticated(req, res, next) {
-  if (!req.session.user) {
-      return res.status(401).json({ message: 'No estás autenticado' });
-  }
-  next();
+    if (!req.session.user) {
+        return res.status(401).json({ message: 'No estás autenticado' });
+    }
+    next();
 }
 
 /**
-* Middleware para verificar si el usuario es administrador
-* @param {Object} req - Objeto de petición
-* @param {Object} res - Objeto de respuesta
-* @param {Function} next - Función para continuar con la siguiente función de middleware
-*/
+ * Middleware para verificar si el usuario es administrador
+ * @param {Object} req - Objeto de petición
+ * @param {Object} res - Objeto de respuesta
+ * @param {Function} next - Función para continuar con la siguiente función de middleware
+ */
 async function isAdmin(req, res, next) {
-  try {
-      if (!req.session.user) {
-          return res.status(401).json({ message: 'No estás autenticado' });
-      }
+    try {
+        if (!req.session.user) {
+            return res.status(401).json({ message: 'No estás autenticado' });
+        }
 
-      const userRole = req.session.user.rolName;
-      if (userRole === 'administrador') {
-          next();
-      } else {
-          return res.status(403).json({ message: 'No tienes permisos para acceder a este recurso' });
-      }
-  } catch (error) {
-      console.log("Error en auth.middleware.js -> isAdmin(): ", error);
-      return res.status(500).json({ message: 'Error interno del servidor' });
-  }
+        const userRole = req.session.user.rolName;
+
+        if (userRole === 'administrador') {
+            next();
+        } else {
+            return res.status(403).json({ message: 'No tienes permisos para acceder a este recurso' });
+        }
+    } catch (error) {
+        console.log("Error en auth.middleware.js -> isAdmin(): ", error);
+        return res.status(500).json({ message: 'Error interno del servidor' });
+    }
 }
 
 export { isAuthenticated, isAdmin };
